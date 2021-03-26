3bbd014b8417d065d1cd6f196613a9471e364fa9
cgiboulot <clement.giboulot@erudo.fr>
2021-03-26 09:57:31 +0100

    Réorganisation multi-game

---
+++ QuenVas/game.py
@@ -0,0 +1,11 @@
+from pygame import Surface
+
+class Game:
+    
+    should_run = True
+
+    def init(self, screen: Surface):
+        return
+
+    def update(self):
+        return
---
+++ QuenVas/main.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame import event, display
+from game import Game
+
+pygame.init()
+display.set_caption("QuenVas")
+
+screen = display.set_mode((800, 600))
+game = Game()
+
+game.init(screen)
+
+while game.should_run :
+    for e in event.get():
+        if e.type == pygame.QUIT:
+            game.should_run = False
+    
+    screen.fill((0, 0, 0))
+    game.update()
+    display.update()
