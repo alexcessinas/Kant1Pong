3bcd8f01876f80daf7b420806c08551ecaec8bb3
cgiboulot <clement.giboulot@erudo.fr>
2021-03-26 10:30:50 +0100

    QuenVas fonctionnel

---
+++ QuenVas/constants.py
@@ -0,0 +1,20 @@
+grid_size = 30
+cell_size = 30
+
+colors = [
+    'white',
+    'grey',
+    'black',
+    'pink',
+    'red',
+    'orange',
+    'brown',
+    'yellow',
+    'lime',
+    'green',
+    'aqua',
+    'dark_aqua',
+    'blue',
+    'mauve',
+    'purple'
+]
--- QuenVas/game.py
+++ QuenVas/game.py
@@ -1,11 +1,17 @@
-from pygame import Surface
+from pygame import Surface, mouse
+from constants import colors
+from player import Player
 
 class Game:
     
+    player = Player()
+
     should_run = True
 
     def init(self, screen: Surface):
+        self.player.init(screen)
         return
 
     def update(self):
+        self.player.update()
         return
